99d3046904a9229f14029cba68217c2032223d00
Vijay Kumar Raheja <vijayrhj@gmail.com>
2018-07-09 11:54:43 +0530

    Changes

--- NearAndBest.DTOs/Admin/RoleDTO.cs
+++ NearAndBest.DTOs/Admin/RoleDTO.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 
 namespace NearAndBest.DTOs.Admin
 {
+    // testing by Vijay
     public class RoleDTO : BaseDTO
     {
         public string RoleName { get; set; }
